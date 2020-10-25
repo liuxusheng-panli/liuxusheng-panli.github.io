a7911c1f0accde2ee9d0a24072644e1706cd56d2
liuxusheng <1213192904@qq.com>
2020-10-25 21:18:20 +0800

    add todo list proj

---
+++ todo-list/precache-manifest.bd8bddfb3a9ef2b975a95c48ebdf7d31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927636bf53b6ea5e65877b47fdb1c0d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec4087d0ee5281da2515",
+    "url": "./static/css/main.60e81819.chunk.css"
+  },
+  {
+    "revision": "3f1324ce06d6c8cf7739",
+    "url": "./static/js/2.93c958da.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.93c958da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4087d0ee5281da2515",
+    "url": "./static/js/main.eee19215.chunk.js"
+  },
+  {
+    "revision": "2cebae45e93442002eab44341159cddb",
+    "url": "./static/js/main.eee19215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd1588001aa7a2ebbd7",
+    "url": "./static/js/runtime-main.e13da3c8.js"
+  }
+]);
