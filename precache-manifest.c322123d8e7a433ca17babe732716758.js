1c3bfde6b42d6f917064131f23e10ae8e25214a2
liuxusheng <1213192904@qq.com>
2020-10-25 21:02:27 +0800

    first commit

---
+++ precache-manifest.c322123d8e7a433ca17babe732716758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387d931ddd3204f20232430b59b12954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "845e4bb70d2f9fa03edc",
+    "url": "/static/js/2.af96e438.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af96e438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829514b1391137c70684",
+    "url": "/static/js/main.68fa966e.chunk.js"
+  },
+  {
+    "revision": "1f00107630985de46dd3",
+    "url": "/static/js/runtime-main.308064c5.js"
+  }
+]);
